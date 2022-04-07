ab32b18c4e86ec6caf78365de7a072584869418b
benjamin huang <benjaminhuanghuang@gmail.com>
2022-04-06 22:42:30 -0700

    remove types

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,10 +8,28 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Users from "./components/Users";
 import Landing from "./components/Landing";
 
+import { setContext } from "apollo-link-context";
+
+const httpLink = new HttpLink({ uri: "http://localhost:8964" });
+
+const authLink = setContext(async (req, { headers }) => {
+	const token = localStorage.getItem("token")
+
+	return {
+		...headers,
+		headers: {
+			Authorization: token ? `Bearer ${token}` : null
+		}
+	}
+})
+
+const link = authLink.concat(httpLink as any)
 const client = new ApolloClient({
-  uri: "http://localhost:8964",
-  cache: new InMemoryCache(),
-});
+	link: link as any,
+	cache: new InMemoryCache()
+})
+
+
 
 function App() {
   return (
